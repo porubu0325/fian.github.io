205f085d3c81cf160f594ac6f20b5858a5ffb203
porubu <hobymour@gmail.com>
2023-11-24 06:44:17 +0900

    Initial Release

---
+++ lang.js
@@ -0,0 +1,7 @@
+
+        <nav>
+            <ul>
+                <li><a href="#" onclick="changeLanguage('en')">English</a></li>
+                <li><a href="#" onclick="changeLanguage('fr')">Français</a></li>
+            </ul>
+        </nav>
